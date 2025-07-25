fbdf9ee9b9ca289e3575df97efc718433bb444fd
felipeazsantos <felipeasantos10@hotmail.com>
2025-07-25 13:15:56 -0300

    closing the auction

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,10 +2,13 @@ package auction
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/felipeazsantos/concurrency-golang-fullcycle-lab03/configuration/logger"
 	auctionentity "github.com/felipeazsantos/concurrency-golang-fullcycle-lab03/internal/entity/auction_entity"
 	internalerror "github.com/felipeazsantos/concurrency-golang-fullcycle-lab03/internal/internal_error"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -46,5 +49,27 @@ func (ar *AuctionRepository) CreateAuction(ctx context.Context, auctionEntity *a
 		return internalerror.NewInternalServerError("Error trying to insert auction ")
 	}
 
+	go func() {
+		select {
+		case <-time.After(getAuctionInterval()):
+			update := bson.M{"$set": bson.M{"status": auctionentity.Completed}}
+			filter := bson.M{"_id": auctionEntityMongo.Id}
+
+			_, err := ar.Collection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				logger.Error("error trying to update auction status to completed", err)
+				return
+			}
+	}()
+
 	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERNAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return 5 * time.Minute
+	}
+	return duration
 }
